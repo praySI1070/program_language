83a841ff266ba9ac76b9afacfa1d7766cdaab232
user02 <user02@DESKTOP-6DN2DTM>
2023-11-02 17:35:33 +0900

    20231102

---
+++ test01/29_3.array_sizeof.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	int score[5];
+	int i;
+	int tot = 0;
+	double avg;
+	int cnt;
+
+	cnt = sizeof(score) / sizeof(score[0]);
+
+	for (i = 0; i < cnt; i++)
+	{
+		scanf("%d", score + i);
+	}
+
+	for (i = 0; i < cnt; i++)
+	{
+		tot += score[i];
+	}
+	avg = tot / (double)cnt;
+
+	for (i = 0; i < cnt; i++)
+	{
+		printf("%5d", score[i]);
+	}
+	printf("\n");
+
+	printf("%.1lf\n", avg);
+
+	return 0;
+}
