4da163f67945f6a3936398fa5912f7c3b372b33f
praySI1070 <145953683+praySI1070@users.noreply.github.com>
2024-11-05 20:47:53 +0900

    Add files via upload

---
+++ cpp/lock/thread_shared_header.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <stdint.h>
+#include <mutex>
+
+using namespace std;
+
+#define print(data)\
+	cout << data << endl;
+
+
+#define int8 int8_t
+#define int16 int16_t
+#define int32 int32_t
+#define int64 int64_t
+#define uint8 uint8_t
+#define uint16 uint16_t
+#define uint32 uint32_t
+#define uint64 uint64_t
