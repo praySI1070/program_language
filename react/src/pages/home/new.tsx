375ff65314cb490737f5063b7a19051569222c07
praySI1070 <145953683+praySI1070@users.noreply.github.com>
2024-11-18 20:46:23 +0900

    Delete react/src directory

--- react/src/pages/home/new.tsx
+++
@@ -1,9 +0,0 @@
-import './new.css';
-
-export default function New() {
-    return(
-        <div className='new-container'>
-            <div className='new-title'>새소식</div>
-        </div>
-    );
-}
