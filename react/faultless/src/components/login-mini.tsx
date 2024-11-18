0a3a05c52a85a22df8ec85c01ae5b34eeac04795
praySI1070 <145953683+praySI1070@users.noreply.github.com>
2024-11-18 20:49:31 +0900

    Add files via upload

---
+++ react/faultless/src/components/login-mini.tsx
@@ -0,0 +1,44 @@
+import "./login-mini.css";
+import {Link} from "react-router-dom";
+import { useLogIn } from "./logincontext";
+
+export default function LoginMini() {
+    const {setLogIn,setUserId,setUserRank,setUserSignDate} = useLogIn();
+
+    const CheckLogin = () => {
+        const inputIdElement = document.querySelector('.id-textbox') as HTMLInputElement; // 타입 단언
+        const id = inputIdElement?.value;
+
+        const inputPwElement = document.querySelector('.password-textbox') as HTMLInputElement;
+        const pw = inputPwElement?.value;
+
+        console.log("test성공!");
+        fetch("http://localhost:6500/Login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id, pw})
+        })
+         .then(res=>res.json())
+         .then(data=> {
+            if(data.userIsLogin) {
+                setLogIn(true);
+                setUserId(id);
+                setUserRank(data.userRank);
+                setUserSignDate(data.userSign);
+            }
+        });
+    };
+
+    return (
+        <div className="loginmini-main">
+            <div className="invisible-box"></div>
+            <div className="id">로그인</div>
+            <input type="text" placeholder="  ID" className="id-textbox"/>
+            <input type="text" placeholder="  PASSWORD" className="password-textbox"/>
+            <Link to="/signup"><div className="signup-link">회원가입</div></Link>
+            <button className="login-button" onClick={CheckLogin}>로그인</button>
+        </div>
+    );
+}
---
+++ react/faultless/src/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import { Routes, BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Loading from '../src/components/Loading';
+
+const Index = () => {
+  const [loading, setLoading] = useState<boolean>(true); // 로딩 상태 관리
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // 2초 후 로딩 상태 변경
+    }, 2000);
+
+    return () => clearTimeout(timer); // 타이머 정리
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <Loading /> // 로딩 중일 때 Loading 컴포넌트를 렌더링
+      ) : (
+        <App /> // 로딩이 끝나면 App 컴포넌트를 렌더링
+      )}
+    </div>
+  );
+};
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Router>
+    <React.StrictMode>
+      <Index />
+    </React.StrictMode>
+  </Router>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
